a5bd099c17884c99a6d094c0b965fbbbff9e46f6
David Krejci <taavetti.krejci@seznam.cz>
2022-01-22 17:57:41 +0100

    created rich text formatter component

---
+++ frontend/src/components/RichTextEditor.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <tiptap-vuetify 
+    :card-props="{ outlined, color: 'transparent' }" 
+    v-model="content" 
+    :extensions="extensions"
+    :toolbar-attributes="{ outlined, color: this.toolbarColor, dark: $vuetify.theme.dark }"
+    :placeholder="placeholder"
+     />
+  </div>
+</template>
+
+<script>
+import {
+  TiptapVuetify,
+  Heading,
+  Bold,
+  Italic,
+  Strike,
+  Underline,
+  Code,
+  Paragraph,
+  BulletList,
+  OrderedList,
+  ListItem,
+  Link,
+  Blockquote,
+  HardBreak,
+  HorizontalRule,
+  History,
+} from "tiptap-vuetify";
+
+export default {
+  name: "RichTextEditor",
+  components: { TiptapVuetify },
+  props: ['placeholder'],
+  data: () => ({
+    extensions: [
+      History,
+      Blockquote,
+      Link,
+      Underline,
+      Strike,
+      Italic,
+      ListItem,
+      BulletList,
+      OrderedList,
+      [
+        Heading,
+        {
+          options: {
+            levels: [1, 2, 3],
+          },
+        },
+      ],
+      Bold,
+      Code,
+      HorizontalRule,
+      Paragraph,
+      HardBreak,
+    ],
+    content: "",
+  }),
+  computed: {
+    toolbarColor: function () {
+        return this.$vuetify.theme.dark ? '#2F333E' : '#F6F7F8';
+      }
+  }
+};
+</script>
--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,31 +1,39 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import { TiptapVuetifyPlugin } from 'tiptap-vuetify'
+import 'tiptap-vuetify/dist/main.css'
 
-Vue.use(Vuetify);
-
-export default new Vuetify({
-    theme: {
-      dark: false,
-      options: {
-        customProperties: true,
-      },
-        themes: {
-          dark: {
+const vuetify = new Vuetify({
+  theme: {
+    dark: false,
+    options: {
+      customProperties: true,
+    },
+      themes: {
+        dark: {
+          primary: '#488FEF',
+          secondary: '#364FCC',
+          accent: '#82B1FF',
+          success: '#27D086',
+          warning: '#FADB39',
+          error: '#FB4444'
+        },
+        light: {
             primary: '#488FEF',
             secondary: '#364FCC',
             accent: '#82B1FF',
             success: '#27D086',
             warning: '#FADB39',
             error: '#FB4444'
-          },
-          light: {
-              primary: '#488FEF',
-              secondary: '#364FCC',
-              accent: '#82B1FF',
-              success: '#27D086',
-              warning: '#FADB39',
-              error: '#FB4444'
-          }
-        },
+        }
       },
+    },
 });
+
+Vue.use(Vuetify);
+Vue.use(TiptapVuetifyPlugin, {
+  vuetify,
+  iconsGroup: 'mdi'
+})
+
+export default vuetify;
