1d982030a6428e90926a2c4cc470833457e2f465
David Krejci <taavetti.krejci@seznam.cz>
2022-01-22 17:50:50 +0100

    added mock data for questions/tags/users

--- frontend/src/api/mock/index.js
+++ frontend/src/api/mock/index.js
@@ -1,13 +1,28 @@
 import courses from './data/courses'
+import users from './data/users'
+import questions from './data/questions'
+import tags from './data/tags';
+
+export default {
+  fetchCourses () {
+    return fetch(courses, 1000)
+  },
+  fetchUsers () {
+    return fetch(users, 1000)
+  },
+  fetchQuestions () {
+    return fetch(questions, 1000)
+  },
+  fetchTags () {
+    return fetch(tags, 1000)
+  }
+}
+
+// Function to simulate loading times with setTimeout
 const fetch = (mockData, time = 0) => {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(mockData)
     }, time)
   })
-}
-export default {
-  fetchCourses () {
-    return fetch(courses, 1000) // wait 1s before returning courses
-  }
 }
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -6,11 +6,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    courses : {}
+    courses : [],
+    users: [],
+    questions: [],
+    tags: []
   },
   mutations: {
     setCourses (state, courses) {
       state.courses = courses
+    },
+    setUsers (state, users) {
+      state.users = users
+    },
+    setQuestions (state, questions) {
+      state.questions = questions
+    },
+    setTags (state, tags) {
+      state.tags = tags
     }
   },
   actions: {
@@ -18,7 +30,22 @@ export default new Vuex.Store({
       return client
         .fetchCourses()
         .then(courses => commit('setCourses', courses))
-    }
+    },
+    fetchUsers ({ commit }) {
+      return client
+        .fetchUsers()
+        .then(users => commit('setUsers', users))
+    },
+    fetchQuestions ({ commit }) {
+      return client
+        .fetchQuestions()
+        .then(questions => commit('setQuestions', questions))
+    },
+    fetchTags ({ commit }) {
+      return client
+        .fetchTags()
+        .then(tags => commit('setTags', tags))
+    },
   },
   modules: {
   }
