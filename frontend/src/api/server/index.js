37d66ef5f6b49c2383ef7bf3c2a4281181a3c0e7
David Krejci <taavetti.krejci@seznam.cz>
2022-02-27 21:28:01 +0100

    added server api methods for questions

--- frontend/src/api/server/index.js
+++ frontend/src/api/server/index.js
@@ -1,7 +1,77 @@
-//import axios from 'axios'
+import axios from 'axios'
 
 export default {
-    fetchCourses () {
+    async fetchCourses () {
         // to implement
-    }
+    },
+    async fetchUsers () {
+        // to implement
+    },
+    async fetchQuestions () {
+        try{
+            const url = process.env.VUE_APP_CODETESTER_API_URL + "/questions";
+            const response = await axios.get(url);
+            return response.data
+        }
+        catch(error){
+            console.log(error);
+            return Promise.reject(error);
+        }
+    },
+    async fetchQuestion (id) {
+        try{
+            const url = process.env.VUE_APP_CODETESTER_API_URL + "/questions/" + id;
+            const response = await axios.get(url);
+            return response.data
+        }
+        catch(error){
+            console.log(error);
+            return Promise.reject(error);
+        }
+    },
+    async editQuestion(id, question) {
+        try{
+            const url = process.env.VUE_APP_CODETESTER_API_URL + "/questions/" + id;
+            const response = await axios.put(url, question);
+            console.log(response)
+        }
+        catch(error){
+            console.log(error);
+            return Promise.reject(error);
+        }
+    },
+    async createQuestion(question) {
+        try{
+            const url = process.env.VUE_APP_CODETESTER_API_URL + "/questions";
+            console.log(JSON.stringify(question));
+            let response = await axios.post(url, question);
+            console.log(response)
+        }
+        catch(error){
+            console.log(error.response);
+            return Promise.reject(error);
+        }
+    },
+    async deleteQuestion(id) {
+        try{
+            const url = process.env.VUE_APP_CODETESTER_API_URL + "/questions/" + id;
+            let response = await axios.delete(url);
+            console.log(response)
+        }
+        catch(error){
+            console.log(error);
+            return Promise.reject(error);
+        }
+    },
+    async fetchTags() {
+        try{
+            const url = process.env.VUE_APP_CODETESTER_API_URL + "/tags";
+            const response = await axios.get(url);
+            return response.data
+        }
+        catch(error){
+            console.log(error);
+            return Promise.reject(error);
+        }
+    },
 }
