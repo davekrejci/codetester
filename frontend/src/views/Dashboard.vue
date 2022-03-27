ac587c00978856e5259ad260e66562303c5a2828
David Krejci <taavetti.krejci@seznam.cz>
2022-03-27 14:45:32 +0200

    changed mobile navigation

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,24 +1,24 @@
 <template>
 	<div>
+		<mobile-navigation></mobile-navigation>
 		<navigation-drawer></navigation-drawer>
 		<v-main>
 			<v-container class="" fluid>
 				<router-view :key="$route.fullPath"></router-view>
 			</v-container>
-		<bottom-navigation></bottom-navigation>
 		</v-main>
 	</div>
 </template>
 
 <script>
 import NavigationDrawer from '@/components/NavigationDrawer.vue';
-import BottomNavigation from '@/components/BottomNavigation.vue';
+import MobileNavigation from '@/components/MobileNavigation.vue';
 
 
 export default {
 	components: {
 		NavigationDrawer,
-		BottomNavigation,
+		MobileNavigation,
 	},
 };
 </script>
