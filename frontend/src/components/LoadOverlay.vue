0db7e099d68a1d4b427b2826bb06eb2bb40c4cfa
David Krejci <taavetti.krejci@seznam.cz>
2022-04-04 19:48:45 +0200

    new load overlay component

---
+++ frontend/src/components/LoadOverlay.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <v-overlay absolute>
+    <v-progress-circular indeterminate size="64"></v-progress-circular>
+  </v-overlay>
+</div>
+</template>
+
+<script>
+export default {
+    name: "LoadOverlay",
+};
+</script>
+
+<style>
+</style>
