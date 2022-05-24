37fd947d43b9483abe3e9c29cecbdaa1173f6ebf
David Krejci <taavetti.krejci@seznam.cz>
2022-05-24 15:21:39 +0200

    removed tiptapvuetify

--- ClientApp/src/plugins/vuetify.js
+++ ClientApp/src/plugins/vuetify.js
@@ -1,7 +1,5 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
-import { TiptapVuetifyPlugin } from 'tiptap-vuetify'
-import 'tiptap-vuetify/dist/main.css'
 
 const vuetify = new Vuetify({
   theme: {
@@ -37,9 +35,5 @@ const vuetify = new Vuetify({
 });
 
 Vue.use(Vuetify);
-Vue.use(TiptapVuetifyPlugin, {
-  vuetify,
-  iconsGroup: 'mdi'
-})
 
 export default vuetify;
