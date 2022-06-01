a917be087e52a7fed2b793aed91aa25dd5e8426a
David Krejci <taavetti.krejci@seznam.cz>
2022-06-01 23:02:52 +0200

    uri parse

--- Startup.cs
+++ Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json.Serialization;
+using Npgsql;
 
 namespace Codetester
 {
@@ -28,21 +29,39 @@ namespace Codetester
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         { 
-            var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL");
-            if(connectionString == null) {
+            // because of deployment to heroku, where db connection data might change, need to retrieve from env variable DATABASE_URL and parse to connection string
+            var connectionString = "";
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+            if(databaseUrl != null) {
+                var databaseUri = new Uri(databaseUrl);
+                var userInfo = databaseUri.UserInfo.Split(':');
+                var builder = new NpgsqlConnectionStringBuilder
+                {
+                    Host = databaseUri.Host,
+                    Port = databaseUri.Port,
+                    Username = userInfo[0],
+                    Password = userInfo[1],
+                    Database = databaseUri.LocalPath.TrimStart('/')
+                };
+                connectionString = builder.ToString();
+            }
+            else {
                 connectionString = Configuration.GetConnectionString("CodetesterConnection");
             }
             services.AddDbContext<CodetesterContext>(opt => opt.UseNpgsql(connectionString));
+            
             services.AddControllers().AddNewtonsoftJson(s =>
             {
                 s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
                 s.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
             });
+            
             // In production, the Vue files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+            
             services.AddAutoMapper((serviceProvider, automapper) =>
             {
                 automapper.AddCollectionMappers();
@@ -56,6 +75,7 @@ namespace Codetester
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Codetester", Version = "v1" });
             });
+            
             services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
             {
                 builder.WithOrigins("http://localhost:8080")
@@ -65,6 +85,7 @@ namespace Codetester
                     .AllowAnyMethod()
                     .AllowAnyHeader();
             }));
+            
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options => {
                     options.TokenValidationParameters = new TokenValidationParameters
